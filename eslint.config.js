978c2af2a4c54e478b2e7aa311062f240e63de66
xxxasdfghjk <xxxasdfghjk999@gmail.com>
2024-06-29 21:55:31 +0900

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -5,16 +5,17 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...fixupConfigRules(pluginReactConfig),
-    {
-        rules: {
-            "react/react-in-jsx-scope": "off",
-            "@typescript-eslint/no-namespace": "off",
-        },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-namespace": "off",
     },
+  },
+  { extends: [["prettier"]] },
 ];
